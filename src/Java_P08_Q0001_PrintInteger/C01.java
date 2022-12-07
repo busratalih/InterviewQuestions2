aa6434ba56b2ff607fcb2aeb124c99e4ac956fa5
Büşra Talih <busra.talihhh@gmail.com>
2022-12-07 21:43:13 +0300

    first commit

---
+++ src/Java_P08_Q0001_PrintInteger/C01.java
@@ -0,0 +1,25 @@
+package Java_P08_Q0001_PrintInteger;
+
+import java.util.Scanner;
+
+public class C01 {
+    public static void main(String[] args) {
+
+        /*
+        Question: Print an integer getting input from user
+         */
+
+        Scanner reader=new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int num= reader.nextInt();
+        System.out.println("you entered" + num);
+
+
+
+
+
+
+
+    }
+}
