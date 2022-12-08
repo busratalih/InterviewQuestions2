1cf2c486bd4692bb4d6fbcb4dc622770764506c3
Büşra Talih <busra.talihhh@gmail.com>
2022-12-08 21:57:06 +0300

    08.12.2022

---
+++ src/Java_P09_Q0002_AddNumbers/C01.java
@@ -0,0 +1,20 @@
+package Java_P09_Q0002_AddNumbers;
+
+public class C01 {
+    public static void main(String[] args) {
+
+      /*
+      Questions : Add two integer
+       */
+
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        System.out.println("sum of a and b is:" + sum);
+
+
+
+
+
+    }
+}
---
+++ src/Java_P09_Q0002_AddNumbers/C02.java
@@ -0,0 +1,20 @@
+package Java_P09_Q0002_AddNumbers;
+
+public class C02 {
+    public static void main(String[] args) {
+
+        /*
+        Question: Multiply floating numbers
+         */
+
+        float f1=2.4f;
+        float f2=3.5f;
+        System.out.println("the product is :" +(f1*f2));
+
+
+
+
+
+
+    }
+}
