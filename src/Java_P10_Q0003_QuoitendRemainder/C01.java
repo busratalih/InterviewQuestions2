9ae724e4442bbb76ed6c17e906e3432fb9fa6473
Büşra Talih <busra.talihhh@gmail.com>
2022-12-10 19:57:23 +0300

    10.12.2022

---
+++ src/Java_P10_Q0003_QuoitendRemainder/C01.java
@@ -0,0 +1,19 @@
+package Java_P10_Q0003_QuoitendRemainder;
+
+public class C01 {
+    public static void main(String[] args) {
+
+        /*
+        Questions: Value of ASCII of characters
+         */
+
+        char c='a';
+        int ascii=c;
+
+        int asciiNumber=(int)c;
+        System.out.println(ascii);
+        System.out.println(asciiNumber);
+
+
+    }
+}
---
+++ src/Java_P10_Q0003_QuoitendRemainder/C02.java
@@ -0,0 +1,19 @@
+package Java_P10_Q0003_QuoitendRemainder;
+
+public class C02 {
+    public static void main(String[] args) {
+
+      int dividend=30;
+      int division=4;
+
+      int quotient=dividend/division;
+      int remainder=dividend%division;
+
+        System.out.println(quotient);
+        System.out.println(remainder);
+
+
+
+
+    }
+}
