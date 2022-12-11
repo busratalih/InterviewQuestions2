1f76ca19011a4362070c43a4d384246d64cfd4ed
Büşra Talih <busra.talihhh@gmail.com>
2022-12-11 20:11:26 +0300

    11.12.2022

---
+++ src/Java_P11_Q0004_SwapNumber/C01.java
@@ -0,0 +1,28 @@
+package Java_P11_Q0004_SwapNumber;
+
+public class C01 {
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=20;
+        System.out.println("before swapping :");
+        System.out.println("Value of a :" + a);
+        System.out.println("Value of b :" + b);
+
+        System.out.println("after swapping");
+
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+
+        System.out.println("Value of a :" + a);
+        System.out.println("Value of b :" + b);
+
+
+
+
+
+
+    }
+}
